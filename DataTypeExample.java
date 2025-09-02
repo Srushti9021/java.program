a6746d3c4811c1c72fe8c9de7b0233aa99074d4a
Srushti9021 <rekhapardeshi4211@gmail.com>
2025-09-02 10:19:04 +0530

    Basic Program of java
    This Program is related to datatype which is use to store data in memory. In that program I use integer , Double , Boolean , Character, String, this type of datatypes are present in program .

---
+++ DataTypeExample.java
@@ -0,0 +1,16 @@
+public class DataTypeExample {
+
+    public static void main(String[] args) {
+        int age = 30;
+        double height = 5.9;
+        boolean isStudent = false;
+        char gender = 'M';
+        String name = "David";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Student: " + isStudent);
+        System.out.println("Gender: " + gender);
+    }
+}
