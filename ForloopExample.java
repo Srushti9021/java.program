67937a5fe3c1394a0c4fd2953bfd5388a3c7f1ad
Srushti9021 <rekhapardeshi4211@gmail.com>
2025-09-19 11:29:41 +0530

    git add-Forloop.java
    "Added for loop program to print numbers from (1-10) and multiplication table of 5 ."

---
+++ ForloopExample.java
@@ -0,0 +1,14 @@
+public class ForloopExample {
+    public static void main(String[] args) {
+        System.out.println("Numbers from (1-10): ");
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+System.out.println("\nTable of 5:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("5 x " + i + " = " + (5 * i));
+        }
+    }
+}
+    
